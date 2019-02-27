db6f91661c00ef000ac9abea5e01b3d218af377b
Rudy <RodriguezLucha@gmail.com>
2019-02-27 09:34:37 -0800

    Add seed file with first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+User.create!(
+  firstname: 'Test',
+  lastname: 'Tester',
+  email: 'test@test.com',
+  password: 'password'
+)
