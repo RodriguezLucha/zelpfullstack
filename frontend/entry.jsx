a3edee0f6afda4cc4813f006c1b599f2daee524e
Rudy <RodriguezLucha@gmail.com>
2019-02-26 18:34:47 -0800

    Add basic front end components and routes for login and sign up

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,26 @@
+// frontend/bench_bnb.jsx
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>React is working</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
