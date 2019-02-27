3acddde7ee4331b4043d0887826abc0361f7f53f
Rudy <RodriguezLucha@gmail.com>
2019-02-26 17:26:22 -0800

    Add user table

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("Loading react.")
   const root = document.getElementById('root');
   ReactDOM.render(<h1>React is working</h1>, root);
 });
