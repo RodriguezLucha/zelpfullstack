f973ba52e29fdd8c5b8804cf7e54a63920089813
Rudy <RodriguezLucha@gmail.com>
2019-02-27 12:59:09 -0800

    Add background image

--- frontend/components/top_restaurants.jsx
+++ frontend/components/top_restaurants.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TopRestaurants = () => (
   <div className="top_restaurants">
-    <h1>Top Restaurants</h1>
+    <h1>Top Restaurants Coming Soon!</h1>
   </div>
 );
 
