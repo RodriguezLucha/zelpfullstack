8360485bba84e99df264bd3a967365d4d96ab4b1
Rudy <RodriguezLucha@gmail.com>
2019-02-28 20:52:45 -0800

    Adding button ready for dropdown

--- frontend/components/home_nav/logged_in.jsx
+++ frontend/components/home_nav/logged_in.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export const LoggedIn = ({currentUser, logout}) => (
   <div className='home_session_links'>
+    <img src="https://s3-media3.fl.yelpcdn.com/assets/srv0/yelp_styleguide/bf5ff8a79310/assets/img/default_avatars/user_medium_square.png" alt="profile"/>
+    <button id="profile_button">
+      <i id="caret-down" className="fas fa-caret-down"></i>
+    </button>
     <div className='home_personal_greeting'>
       Hi, {currentUser.firstname}!
     </div>
