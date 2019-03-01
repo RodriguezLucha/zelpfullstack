3c3a49f6415379f895b0db6ac13f55719c673c61
Rudy <RodriguezLucha@gmail.com>
2019-03-01 09:17:14 -0800

    Add dropdown for test user

--- frontend/components/home_nav/logged_in.jsx
+++ frontend/components/home_nav/logged_in.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export const LoggedIn = ({currentUser, logout}) => (
   <div className='home_session_links'>
-    <img src="https://s3-media3.fl.yelpcdn.com/assets/srv0/yelp_styleguide/bf5ff8a79310/assets/img/default_avatars/user_medium_square.png" alt="profile"/>
-    <button id="profile_button">
-      <i id="caret-down" className="fas fa-caret-down"></i>
-    </button>
+    <div className="dropdown">
+      <div className="dropbtn">
+        <img src="https://s3-media3.fl.yelpcdn.com/assets/srv0/yelp_styleguide/bf5ff8a79310/assets/img/default_avatars/user_medium_square.png" alt="profile" />
+        <button id="profile_button">
+          <i id="caret-down" className="fas fa-caret-down"></i>
+        </button>
+      </div>
+      <div className="dropdown_content">
+        <div className='dropdown_top'>
+          <img src="https://s3-media3.fl.yelpcdn.com/assets/srv0/yelp_styleguide/bf5ff8a79310/assets/img/default_avatars/user_medium_square.png" alt="profile" />
+          <strong id="drp_name">
+            {currentUser.firstname}
+          </strong>
+        </div>
+        <hr/>
+        <button onClick={logout}>Log Out</button>
+      </div>
+    </div>
+
     <div className='home_personal_greeting'>
       Hi, {currentUser.firstname}!
     </div>
