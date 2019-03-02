55c9859d30de4d0db5409dffb4dcaf1413e87fe7
Rudy <RodriguezLucha@gmail.com>
2019-03-02 12:49:24 -0800

    Updating styles on the forms

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Footer = () => (
   <div className="footer">
     <img src="https://s3-media2.fl.yelpcdn.com/assets/srv0/yelp_styleguide/573fa19c8435/assets/img/structural/footer_cityscape.png" alt="footer_cityscape"/>
+    
   </div>
 );
 
