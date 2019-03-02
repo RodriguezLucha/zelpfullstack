5709e91441969bf9d9acdc35b2aefa5c2ecbd97e
Rudy <RodriguezLucha@gmail.com>
2019-03-01 16:25:08 -0800

    Minor updates

--- frontend/components/home_nav/logged_out.jsx
+++ frontend/components/home_nav/logged_out.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export const LoggedOut = ({ props }) => (
   <div className="home_session_links">
-    <Link id="login_button" to="/login">Login</Link>
+    <Link id="login_button" to="/login">Log In</Link>
     <br></br>
     <Link id="signup_button" to="/signup">Sign up!</Link>
     <button id="demo_button" onClick={() => props.loginDemoUser(props.history)}>
