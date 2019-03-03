7012e97aa2e0fd6872b2279ffccced9cdab3ffea
Rudy <RodriguezLucha@gmail.com>
2019-03-02 16:21:49 -0800

    Update padding for nav buttons

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomeNav from './home_nav/home_nav_container';
 
-//<i class="fas fa-wine-glass-alt"></i>
-
 const Home = ({props}) => (
   <div className="home">
-    <HomeNav props={props}/>
+    <div className="nav_container_outer">
+      <div className="nav_container_inner">
+        <HomeNav props={props}/>
+      </div>
+    </div>
     <div className="home_banner">
       <h1>Zelp</h1>
     </div>
