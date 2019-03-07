f0adcbc6dea5ee7f0f0991c16fb0135eb0052592
Rudy <RodriguezLucha@gmail.com>
2019-03-06 19:56:26 -0800

    More styling for search results, stars now change color based on rating

--- frontend/components/restaurant/star_ratings.jsx
+++ frontend/components/restaurant/star_ratings.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 function StarRatings({starCount}) {
   let stars = [];
 
+  let color = starCount <= 3 ? 'yellow' : 'red';
+
   for (let i = 0; i < starCount; i++) {
-    stars.push(<div className="mystar fa fa-star checked"></div>);
+    stars.push(<div className={`mystar fa fa-star checked ${color}`}></div>);
   }
   for (let i = starCount; i < 5; i++) {
-    stars.push(<div className="mystar fa fa-star"></div>);
+    stars.push(<div className={`mystar fa fa-star ${color}`}></div>);
   }
 
   return (
