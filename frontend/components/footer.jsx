106318013e1ec863db4dd7af2b4efe7444ed80bb
Rudy <RodriguezLucha@gmail.com>
2019-04-05 09:19:02 -0700

    Add github and linked in links in footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Footer = () => (
   <div className="footer">
-    <img src="https://s3-media2.fl.yelpcdn.com/assets/srv0/yelp_styleguide/573fa19c8435/assets/img/structural/footer_cityscape.png" alt="footer_cityscape"/>
-    
+    <img src="https://s3-media2.fl.yelpcdn.com/assets/srv0/yelp_styleguide/573fa19c8435/assets/img/structural/footer_cityscape.png" alt="footer_cityscape" />
+    <div className="my_links">
+      <a className="github" href="https://github.com/RodriguezLucha/zelpfullstack" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-github"></i>
+      </a>
+      <a className="linkedin" href="https://www.linkedin.com/in/rudy-rodriguez-11424020/" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-linkedin"></i>
+      </a>
+    </div>
   </div>
 );
 
