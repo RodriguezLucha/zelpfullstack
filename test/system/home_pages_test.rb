d2e3de875748177332d67034da49cd9cd8a3613b
Rudy <RodriguezLucha@gmail.com>
2019-05-26 21:41:26 -0700

    Add 2nd test

--- test/system/home_pages_test.rb
+++ test/system/home_pages_test.rb
@@ -5,4 +5,9 @@ class HomePagesTest < ApplicationSystemTestCase
     visit root_url
     assert_selector "h1", text: "Zelp"
   end
+
+  test "Visit the login page" do
+    visit root_url + "#/login"
+    assert_selector "h1", text: "Log In to Zelp"
+  end
 end
