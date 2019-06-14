354551cf07048c0d33d739f8de7f6d9c7015e57b
Rudy <RodriguezLucha@gmail.com>
2019-06-14 13:18:02 -0700

    Add visual regression tests for all main pages

---
+++ end_to_end_tests/visual_tests/visual.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'Home page' : function (browser) {
+    browser
+      .url('http://localhost:3000/')
+      .waitForElementVisible('.top_restaurants')
+      .assert.screenshotIdenticalToBaseline('body', 'home')
+      .assert.screenshotIdenticalToBaseline('.top_restaurants')
+      .assert.screenshotIdenticalToBaseline('.footer');
+  },
+  'Login page' : function (browser) {
+    browser
+      .url('http://localhost:3000/#/login')
+      .waitForElementVisible('body')
+      .assert.screenshotIdenticalToBaseline('body', 'login');
+  },
+  'Single Restaurant' : function (browser) {
+    browser
+      .url('http://localhost:3000/#/restaurant/161')
+      .waitForElementVisible('.single_restaurant')
+      .assert.screenshotIdenticalToBaseline('body', 'single_restaurant');
+  },
+  'Write a Review' : function (browser) {
+    browser
+      .url('http://localhost:3000/#/restaurant/161/review')
+      .waitForElementVisible('body')
+      .assert.screenshotIdenticalToBaseline('body', 'write_a_review');
+  },
+
+};
